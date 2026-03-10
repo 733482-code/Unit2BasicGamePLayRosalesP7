2ca6e9a325128246ea7375e0130c4efb1c819658
733482-code <733482@student.dusd.net>
2026-03-10 15:22:33 -0700

    2.4
    working on 2.4

--- P7BasicGamePlayRosales/Assets/Scripts/SpawnManager.cs
+++ P7BasicGamePlayRosales/Assets/Scripts/SpawnManager.cs
@@ -2,21 +2,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalsPrefabs;
-    public int animalIndex;
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalsPrefabs[animalIndex], new Vector3(0, 0, 20), animalsPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
