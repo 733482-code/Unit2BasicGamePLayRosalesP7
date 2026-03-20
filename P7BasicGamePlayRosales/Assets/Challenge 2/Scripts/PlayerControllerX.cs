3ac23ec684e16e787ece393d17987a12417b2d89
733482-code <733482@student.dusd.net>
2026-03-19 19:29:58 -0700

    Challenge 2 complete
    the spam key is fixed

--- P7BasicGamePlayRosales/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ P7BasicGamePlayRosales/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-    private object Delay;
 
-    
-    // Update is called once per frame
+    // 1. Add these variables to track timing
+    public float spawnInterval = 0.5f;
+    private float nextSpawnTime = 0f;
+
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // 2. Check if space is pressed AND if current time has passed the cooldown
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawnTime)
         {
+            // 3. Set the next allowed spawn time
+            nextSpawnTime = Time.time + spawnInterval;
+
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
 }
+
